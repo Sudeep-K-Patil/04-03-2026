4180d646afc12621195f8bd6f22171bea1b4dffc
Sudeep <sudeeppatil1619@gmail.com>
2026-03-04 10:27:14 +0530

    Implement array reversal in reverse_array.java

---
+++ reverse_array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class reverse_array
+{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3,4, 4, 5};
+        int n = arr.length;
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
